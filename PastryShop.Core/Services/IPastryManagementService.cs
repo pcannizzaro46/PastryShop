8ee69e8844b9324afd983f5ef05ac89697f7d276
Pietro Cannizzaro <80398250+pcannizzaro46@users.noreply.github.com>
2021-03-21 21:52:22 +0100

    Add files via upload

---
+++ PastryShop.Core/Services/IPastryManagementService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PastryShop.Core.Models;
+
+namespace PastryShop.Core.Services
+{
+    public interface IPastryManagementService
+    {
+        DessertModel AddDessert(DessertModel dessert);
+        
+        void DeleteDessert(DessertModel dessert);
+
+        void UpdateDessert(DessertModel dessert);
+        
+        IEnumerable<DessertModel> GetDesserts(bool includeIngredients);
+
+        ShowcaseItemModel SellDessert(DessertModel dessert);
+
+        IEnumerable<ShowcaseItemModel> GetShowcaseItemsOnSale(bool includeIngredients);
+
+        IEnumerable<ShowcaseItemModel> GetAllShowcaseItems(bool includeIngredients);
+
+        Task DeleteShowCaseItem(ShowcaseItemModel showcaseItem);
+
+        Task SaveAsync();
+    }
+}
---
+++ PastryShop.Core/Services/Impl/PastryManagementService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PastryShop.Core.Models;
+using PastryShop.DAL.Interfaces;
+using PastryShop.DAL.Models;
+
+namespace PastryShop.Core.Services.Impl
+{
+    public class PastryManagementService : IPastryManagementService
+    {
+        private readonly IPastryRepository _pastryRepository;
+
+        public PastryManagementService(IPastryRepository pastryRepository)
+        {
+            _pastryRepository = pastryRepository;
+        }
+
+        public DessertModel AddDessert(DessertModel dessert)
+        {
+            _pastryRepository.AddDessert(dessert.Entity);
+            return dessert;
+        }
+
+        public void DeleteDessert(DessertModel dessert) => _pastryRepository.DeleteDessert(dessert.Entity);
+
+        public void UpdateDessert(DessertModel dessert) => _pastryRepository.UpdateDessert(dessert.Entity);
+
+        public IEnumerable<DessertModel> GetDesserts(bool includeIngredients) =>
+            _pastryRepository.GetDesserts(includeIngredients).Select(d => new DessertModel(d));
+
+        public ShowcaseItemModel SellDessert(DessertModel dessert)
+        {
+            if (dessert == null) throw new ArgumentNullException(nameof(dessert));
+
+            var sci = _pastryRepository.AddShowcaseItem(
+                new ShowcaseItem
+                {
+                    Dessert = dessert.Entity,
+                    DessertId = dessert.DessertId,
+                    Price = dessert.Price,
+                    CreateUser = dessert.UserCreate, //TODO: dovrebbe essere l'utente loggato
+                    CreateDate = DateTime.Now
+                });
+
+            return new ShowcaseItemModel(sci);
+        }
+
+        public async Task DeleteShowCaseItem(ShowcaseItemModel showcaseItem)
+        {
+            _pastryRepository.DeleteShowcaseItem(showcaseItem.Entity);
+            //await _pastryRepository.SaveAsync();
+        }
+
+        public IEnumerable<ShowcaseItemModel> GetShowcaseItemsOnSale(bool includeIngredients) =>
+            GetAllShowcaseItems(includeIngredients).Where(showcaseItem => !showcaseItem.IsExpired);
+
+        public IEnumerable<ShowcaseItemModel> GetAllShowcaseItems(bool includeIngredients) =>
+            _pastryRepository.GetShowcaseItems(includeIngredients)
+                .Select(showcaseItem => new ShowcaseItemModel(showcaseItem));
+
+        public Task SaveAsync() => _pastryRepository.SaveAsync();
+    }
+
+    public class ServiceException : Exception
+    {
+        public ServiceException(string message) : base(message)
+        {
+        }
+    }
+}
